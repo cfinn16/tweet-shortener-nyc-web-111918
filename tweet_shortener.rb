3c9d5b8744e412fdf30180605996d1a95ae3c754
Connor Finnegan <connorfinnegan16@gmail.com>
2018-10-30 05:00:16 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,5 +35,5 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  word_substituter(tweet).length > 140 ? "#{word_substituter(tweet)[0..139]}..." : word_substituter(tweet)
+  word_substituter(tweet).length > 140 ? "#{word_substituter(tweet)[0..136]}..." : word_substituter(tweet)
 end
